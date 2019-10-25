87c599a1494cdfe8257c41f6b4ca1b34b0458e62
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 10:28:46 +0200

    check read method on stdin

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def read_data():
     input_data = sys.stdin
-    print(type(input_data))
+    print (type(input_data.read()))
+    print(input_data.read())
     for line in input_data:
         edge = line.split()
         # print(line)
