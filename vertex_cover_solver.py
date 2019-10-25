9ecf84f39506d84cc5f14bdb3daea374fb43ff06
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 10:37:54 +0200

    print line of stdin

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -4,7 +4,8 @@ import numpy as np
 def read_data():
     input_data = sys.stdin
     for line in input_data:
+        print(line)
         edge = line.split()
-        # print(line)
+        # print(edge)
 
 read_data()
