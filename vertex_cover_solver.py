b45cc8202a4a112bbb9929d32ceb2fbb14b366c3
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 11:11:06 +0200

    changed numpy to np

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -6,7 +6,7 @@ def read_data():
     for counter, line in enumerate(input_data):
         if counter == 0:
             num_of_edges = int(line.split()[1])
-            edges = np.empty(num_of_edges, dtype=numpy.ndarray)
+            edges = np.empty(num_of_edges, dtype=np.ndarray)
         else:
             current_edge = np.asarray(map(int, line.split()))       # TODO: maybe convert later to int (in np)
             edges[counter-1] = current_edge
