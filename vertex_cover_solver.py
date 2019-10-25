0bdcefea982ae8c79060054a67ae812fbe4da4bc
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 10:35:36 +0200

    delete input print statements

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -3,8 +3,6 @@ import numpy as np
 
 def read_data():
     input_data = sys.stdin
-    print (type(input()))
-    print(input() + "test")
     for line in input_data:
         edge = line.split()
         # print(line)
