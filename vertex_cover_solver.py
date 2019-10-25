43d71de8d912d62acd0a963a889a78a344b8d2e1
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 11:28:56 +0200

    comment function

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -2,15 +2,27 @@ import sys
 import numpy as np
 
 def get_data():
+    """
+    INPUT: None
+    Function reads standard input
+    OUTPUT: array of all edges
+    """
+    # Get standard input:
     input_data = sys.stdin
     for counter, line in enumerate(input_data):
         if counter == 0:
+            # Extract number of edges from first line:
             num_of_edges = int(line.split()[1])
+            # Initialize array of edges:
             edges = np.empty(num_of_edges, dtype=np.ndarray)
         else:
+            # Get current edge and convert it to int:
             current_edge = list(map(int, line.split()))     # TODO: maybe convert later to int (in np)
+            # Convert edge to numpy array:
             current_edge = np.asarray(current_edge)
+            # Add edge to array of all edges:
             edges[counter-1] = current_edge
+    # Return array of edges:
     return edges
 
 get_data()
