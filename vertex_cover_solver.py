95bb804146e9daa2dd72cff922a5421d60f6952e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 11:09:57 +0200

    completed if else statement and converted to numpy array of numpy arrays

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -5,10 +5,11 @@ def read_data():
     input_data = sys.stdin
     for counter, line in enumerate(input_data):
         if counter == 0:
-            print("special: " + line)
+            num_of_edges = int(line.split()[1])
+            edges = np.empty(num_of_edges, dtype=numpy.ndarray)
         else:
-            print(line)
-        edge = line.split()
-        # print(edge)
+            current_edge = np.asarray(map(int, line.split()))       # TODO: maybe convert later to int (in np)
+            edges[counter-1] = current_edge
+    print(edges)
 
 read_data()
