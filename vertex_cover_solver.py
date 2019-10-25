e69b4487cf7264d7836782f8db51caf8a53b8338
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 10:52:28 +0200

    print whole first line

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -5,7 +5,7 @@ def read_data():
     input_data = sys.stdin
     for counter, line in enumerate(input_data):
         if counter == 0:
-            print("special: " + line[-1])
+            print("special: " + line)
         else:
             print(line)
         edge = line.split()
