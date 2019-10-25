f8737e559cc23013396f03ac85f4836ec9587b30
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 10:32:50 +0200

    try input

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -3,8 +3,8 @@ import numpy as np
 
 def read_data():
     input_data = sys.stdin
-    print (type(input_data.read()))
-    print(input_data.read() + "test")
+    print (type(input()))
+    print(input() + "test")
     for line in input_data:
         edge = line.split()
         # print(line)
