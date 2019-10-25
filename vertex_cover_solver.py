1c31dab5309f5c8833e5bb20e524ae55689b10e5
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-25 10:24:56 +0200

    check type of stdin

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def read_data():
     input_data = sys.stdin
-    print(input_data[0])
+    print(type(input_data))
     for line in input_data:
         edge = line.split()
         # print(line)
