11cd9a000369bdc7f62dca6faf6162a0973cc71e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-30 20:21:01 +0100

    new directory and save in there

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 time_stamp=$(date +%Y%m%d%H%M)
-file_name="${time_stamp}_run_history.txt"
+file_name="run_history/${time_stamp}_run_history.txt"
 echo "benchmark.sh is running..."
 bash benchmark > $file_name
 cat $file_name
