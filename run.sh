e23d2edb170755c4ca5b8f5b7181dde547812e6d
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-30 20:14:31 +0100

    automatically writing in new file with time stamp

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-# TIMESTAMP = $(date +"%Y%m%d%H%M")
-# TIMESTAMP="test"
 time_stamp=$(date +%Y%m%d%H%M)
-echo $time_stamp-run_history.txt
+file_name="${time_stamp}_run_history.txt"
+echo "benchmark.sh is running..."
+bash benchmark > $file_name
+cat $file_name
