5aff6c741c7effb4c9519a80900185314858d5fe
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-30 23:11:10 +0100

    import csv

--- history/process_history.py
+++ history/process_history.py
@@ -1,4 +1,5 @@
 import sys
+import csv
 
 current_path = sys.path[0] + "/"
 current_history_file = "201910302031_run_history."
@@ -11,4 +12,5 @@ for line in history_file:
     # if line starts with random, snap or dimacs
     # line.split()
     with open(current_path + current_history_file + "csv", "a") as sheet:
-        sheet.write('test')
+        writer = csv.writer(sheet)
+        writer.writerow(line.split())
