b8bfb020f241b0694576ba1e91f7f6607c9af90b
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-30 20:25:01 +0100

    change directory name

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 time_stamp=$(date +%Y%m%d%H%M)
-file_name="run_history/${time_stamp}_run_history.txt"
+file_name="history/${time_stamp}_run_history.txt"
 echo "benchmark.sh is running..."
 bash benchmark.sh > $file_name
 cat $file_name
