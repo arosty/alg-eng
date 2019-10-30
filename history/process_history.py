b0ff29b512fa59faba80f97888fc518bd8da056f
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 00:23:04 +0100

    make it working

--- history/process_history.py
+++ history/process_history.py
@@ -4,18 +4,10 @@ import csv
 current_path = sys.path[0] + "/"
 current_history_file = "201910302031_run_history."
 
-history_file = open(current_path + current_history_file + "txt", "r")
+history_file = open(current_path + current_history_file + 'txt', 'r')
 
 for line in history_file:
-    print("NEUE ZEILE")
-    print(line)
-    # if line starts with random, snap or dimacs
-    # line.split()
-    with open(current_path + current_history_file + 'csv', 'a') as sheet:
-        writer = csv.writer(sheet)
-        writer.writerow(line.split())
-    # with open(current_path + current_history_file + 'csv', 'a', newline='') as sheet:
-    #     sheet.write("test")
-
-
-# history_file.close()
+    if line[:4] in ["rand", "dima", "snap"]:
+        with open(current_path + current_history_file + 'csv', 'a') as sheet:
+            writer = csv.writer(sheet)
+            writer.writerow(line.split())
