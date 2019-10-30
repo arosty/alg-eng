89b0ae7504c56d439ef536c86c61b489aabefc9f
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 00:02:57 +0100

    changed path to history directory

--- history/process_history.py
+++ history/process_history.py
@@ -1,16 +1,20 @@
 import sys
-import csv
 
-current_path = sys.path[0] + "/"
+current_path = sys.path[0] + "/history/"
 current_history_file = "201910302031_run_history."
 
 history_file = open(current_path + current_history_file + "txt", "r")
 
 for line in history_file:
-    # print("NEUE ZEILE")
-    # print(line)
+    print("NEUE ZEILE")
+    print(line)
     # if line starts with random, snap or dimacs
     # line.split()
-    with open(current_path + current_history_file + "csv", "a") as sheet:
-        writer = csv.writer(sheet)
-        writer.writerow(line.split())
+    # with open(current_path + current_history_file + "csv", "a") as sheet:
+    #     writer = csv.writer(sheet)
+    #     writer.writerow("test")
+    with open(current_path + current_history_file + 'csv', 'a') as sheet:
+        sheet.write(line.split())
+
+
+# history_file.close()
