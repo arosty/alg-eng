3d0619b40dc57933e2be43db79ec5a576966fe2e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-30 20:49:47 +0100

    read txt file via python

--- history/process_history.py
+++ history/process_history.py
@@ -1,3 +1,8 @@
 import sys
 
-print(sys.path[0])
+current_path = sys.path[0] + "/"
+current_history_file = "201910302031_run_history.txt"
+
+history_file = open(current_path + current_history_file, "r")
+
+print(history_file.read())
