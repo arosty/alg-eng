4cdea6a9514cf5a9dad1949720450d31fada79f9
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 07:45:48 +0100

    get file name from stdin

--- history/process_history.py
+++ history/process_history.py
@@ -4,7 +4,23 @@ import csv
 current_path = sys.path[0] + "/"
 current_history_file = current_path + "201910302257_run_history."
 
+def get_file_name():
+    """
+    INPUT: None
+    get_data reads standard input and returns the given edges
+    OUTPUT: np.array of shape (nb_edges,2)
+    """
+    return input()
+
+print(get_file_name)
+
 def transfer_data(current_history_file):
+    """
+    INPUT: current_history_file is string
+    transfer_data converts data of txt file and writes it into csv file
+    (txt and csv file must have same names)
+    OUTPUT: None
+    """
     # Read txt file:
     history_file = open(current_history_file + 'txt', 'r')
     # Iterate through every line of txt file:
@@ -18,7 +34,7 @@ def transfer_data(current_history_file):
                     writer.writerow(line.split())
                 break
 
-transfer_data(current_history_file)
+# transfer_data(current_history_file)
 
 # standard input
 # being called automatically
