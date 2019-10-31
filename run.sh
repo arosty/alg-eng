56e42dbc808ac4ff68436a377c7d9abd6a177c0b
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 08:00:24 +0100

    write timestamp in txt file

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 time_stamp=$(date +%Y%m%d%H%M)
 file_name="history/${time_stamp}_run_history.txt"
+echo $time_stamp > $file_name
 echo "benchmark.sh is running..."
-bash benchmark.sh > $file_name
+bash benchmark.sh >> $file_name
 cat $file_name
 file_name="history/${time_stamp}_run_history.csv"
 echo >> $file_name
