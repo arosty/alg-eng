3b537fdb0f746bf9613dc1a58de2c5341b721f2e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 07:38:40 +0100

    put code into function and call function

--- history/process_history.py
+++ history/process_history.py
@@ -2,12 +2,23 @@ import sys
 import csv
 
 current_path = sys.path[0] + "/"
-current_history_file = "201910302031_run_history."
+current_history_file = current_path + "201910302257_run_history."
 
-history_file = open(current_path + current_history_file + 'txt', 'r')
+def transfer_data(current_history_file):
+    # Read txt file:
+    history_file = open(current_history_file + 'txt', 'r')
+    # Iterate through every line of txt file:
+    for line in history_file:
+        for starter in ["random/", "dimacs/", "snap/"]:
+            # If row contains statistics:
+            if line.startswith(starter):
+                # Write data in csv file:
+                with open(current_history_file + 'csv', 'a') as sheet:
+                    writer = csv.writer(sheet)
+                    writer.writerow(line.split())
+                break
 
-for line in history_file:
-    if line[:4] in ["rand", "dima", "snap"]:
-        with open(current_path + current_history_file + 'csv', 'a') as sheet:
-            writer = csv.writer(sheet)
-            writer.writerow(line.split())
+transfer_data(current_history_file)
+
+# standard input
+# being called automatically
