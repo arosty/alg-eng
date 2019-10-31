7fa625cbc4b5331b68d68bee02d33b2474bed1f8
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 12:50:08 +0100

    make coloring possible

--- plot_results.py
+++ plot_results.py
@@ -4,13 +4,16 @@ import pandas as pd
 # 0: Paul, 1: Alfons
 current_user = 1
 if current_user == 0:
-    print('')
+    current_path = 'etc'
 elif current_user == 1:
     current_path = 'C:/Users/ARF/Code/arosty/tu_berlin/'
     
 current_path += 'alg-eng/history/'
     
-timestamps = ['201910311157']
+# PUT IN THE TIMESTAMPS OF THE FILES THAT YOU WANT TO COMPARE:
+timestamps = ['201910311157', '201910311239']
+# CHOOSE THE COLORS FOR THE PLOTTER:
+colors = ['red', 'green']
 
 pd.set_option('display.max_columns', None)
 
@@ -25,10 +28,9 @@ for stamp in timestamps:
         new_df[new_df.columns[col]] = pd.to_numeric(new_df[new_df.columns[col]])
     data.append(new_df)
 
-
 # data.plot(kind='scatter', x=data.index, y='time_in_seconds')
 
-for df in data:
-    plt.scatter(df.index, df.time_in_seconds)
+for idx, df in enumerate(data):
+    plt.scatter(df.index, df.time_in_seconds, color=colors[idx])
 plt.ylabel('Time in seconds')
 plt.show()
