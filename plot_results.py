96990729193b300f5d757807869b2495bc195fbf
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 11:56:07 +0100

    added header to csv files

--- plot_results.py
+++ plot_results.py
@@ -1,5 +1,23 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import sys
 
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
-plt.show()
+# 0: Paul, 1: Alfons
+current_user = 1
+if current_user == 0:
+    print('')
+elif current_user == 1:
+    current_path = 'C:/Users/ARF/Code/arosty/tu_berlin/'
+    
+current_path += 'alg-eng/history/'
+    
+current_timestamp = '201910311127'
+
+current_file = current_path + current_timestamp + '_run_history.csv'
+
+data = pd.read_csv(current_file)
+
+def seconds_per_instance_plot:
+    plt.plot([-5, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
