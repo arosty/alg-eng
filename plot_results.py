2595ac668f792792dce862e7bbbe5ebba796142e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-10-31 11:56:49 +0100

    read csv file in plot file

--- plot_results.py
+++ plot_results.py
@@ -3,6 +3,11 @@ import pandas as pd
 import sys
 
 # 0: Paul, 1: Alfons
+
+def seconds_per_instance_plot:
+    plt.plot([-5, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
 current_user = 1
 if current_user == 0:
     print('')
@@ -16,8 +21,3 @@ current_timestamp = '201910311127'
 current_file = current_path + current_timestamp + '_run_history.csv'
 
 data = pd.read_csv(current_file)
-
-def seconds_per_instance_plot:
-    plt.plot([-5, 3, 4])
-    plt.ylabel('some numbers')
-    plt.show()
