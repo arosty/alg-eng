f4a9d9c0f4b7aceb9efda12ca0df2e8597e3a392
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-12-01 23:11:32 +0100

    apply deg2 every 5th time, dom always

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -478,9 +478,12 @@ def kernalization(k):
     S_kern_one, undelete_one, k = degree_one_rule(k)
     S_kern += S_kern_one
     undelete += undelete_one
-    # S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
-    # S_kern += S_kern_two
-    # undelete += undelete_two
+    if vc_branch.counter % 5 == 0:
+        S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
+        S_kern += S_kern_two
+        undelete += undelete_two
+    else:
+        unmerge = []
     S_kern_dom, undelete_dom, k = domination_rule(k)
     S_kern += S_kern_dom
     undelete += undelete_dom
