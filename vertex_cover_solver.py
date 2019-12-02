0656a370f71983827cf98d9561012cda1b94c8fc
Paul <paul.binetruy@centrale-marseille.fr>
2019-12-02 01:55:15 +0100

    branch_step_64

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -479,13 +479,14 @@ def kernalization(k, branching_step):
     S_kern_one, undelete_one, k = degree_one_rule(k)
     S_kern += S_kern_one
     undelete += undelete_one
-    # if (branching_step % 10) == 0:
-    #     S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
-    #     S_kern += S_kern_two
-    #     undelete += undelete_two
-    S_kern_dom, undelete_dom, k = domination_rule(k)
-    S_kern += S_kern_dom
-    undelete += undelete_dom
+    if (branching_step % 20) == 1:
+        S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
+        S_kern += S_kern_two
+        undelete += undelete_two
+    if (branching_step % 10) == 2:
+        S_kern_dom, undelete_dom, k = domination_rule(k)
+        S_kern += S_kern_dom
+        undelete += undelete_dom
     return S_kern, undelete, unmerge, k
 
 
