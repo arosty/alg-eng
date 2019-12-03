d54db7e57db8d4a0df22518cb3c6e0ea86348924
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-12-03 19:36:46 +0100

    apply dom every 2nd time

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -482,9 +482,10 @@ def kernalization(k):
     S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
     S_kern += S_kern_two
     undelete += undelete_two
-    # S_kern_dom, undelete_dom, k = domination_rule(k)
-    # S_kern += S_kern_dom
-    # undelete += undelete_dom
+    if vc_branch.counter % 2 == 0:
+        S_kern_dom, undelete_dom, k = domination_rule(k)
+        S_kern += S_kern_dom
+        undelete += undelete_dom
     return S_kern, undelete, unmerge, k
 
 
