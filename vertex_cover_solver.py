186d81438ad51fff15c08e7789a02eaedbfabac5
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-12-04 00:54:30 +0100

    disabled deg2

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -479,12 +479,10 @@ def kernalization(k):
     S_kern_one, undelete_one, k = degree_one_rule(k)
     S_kern += S_kern_one
     undelete += undelete_one
-    if vc_branch.counter % 5 == 0:
-        S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
-        S_kern += S_kern_two
-        undelete += undelete_two
-    else:
-        unmerge = []
+    # S_kern_two, undelete_two, unmerge, k = degree_two_rule(k)
+    # S_kern += S_kern_two
+    # undelete += undelete_two
+    unmerge = []
     S_kern_dom, undelete_dom, k = domination_rule(k)
     S_kern += S_kern_dom
     undelete += undelete_dom
