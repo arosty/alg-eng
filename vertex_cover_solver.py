5c0002ea028ab91145060b0aabfb27dc8cfde64a
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2019-12-11 17:03:28 +0100

    debugged

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -420,10 +420,12 @@ def basic_rules(k):
     S_kern, undelete = [], []
     while True:
         S_kern_ex, undelete_ex, k = extreme_reduction_rule(k)
+        S_kern += S_kern_ex
+        undelete += undelete_ex
         if k < 0: break
         S_kern_one, undelete_one, k = degree_one_rule(k)
-        S_kern += S_kern_ex + S_kern_one
-        undelete += undelete_ex + undelete_one
+        S_kern += S_kern_one
+        undelete += undelete_one
         if (S_kern_ex == [] and S_kern_one == []) or k < 0: break
     return S_kern, undelete, k
 
