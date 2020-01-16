a9dee5a894866021b3e802d63d94f644f417fa1b
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-01-16 14:59:04 +0100

    debug print

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -546,6 +546,12 @@ def lp_rule(k):
     # prob.parameters.mip.tolerances.absmipgap = 1e-15
     #fill the CPLEX problem with all correct parameters
     prob.objective.set_sense(prob.objective.sense.minimize)
+    # print(my_colnames)
+    # print(my_rownames)
+    print(nb_edges)
+    print(nb_vertices)
+    print(len(my_colnames))
+    print(len(my_rownames))
     prob.variables.add(obj=my_obj, ub=my_ub, types=my_ctype, names=my_colnames)
     prob.linear_constraints.add(lin_expr=rows, senses=my_sense, rhs=my_rhs, names=my_rownames)
     #Solve the CPLEX problem
