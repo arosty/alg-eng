8b080968b00a3e3925b58bcca30066f11d34fed7
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-01-27 11:17:04 +0100

    600 sec per instance

--- benchmark.sh
+++ benchmark.sh
@@ -111,7 +111,7 @@ run_ce_solver()
 PROGRAMM_NAME="python3 vertex_cover_solver.py"  	# insert your program here
 LOG=log.txt									# specify the name of the log file
 maxSec=43200									# overall allowed time for the whole script
-maxSecPerInstance=300							# allowed time (in seconds) for one instance
+maxSecPerInstance=600							# allowed time (in seconds) for one instance
 maxNotSolved=10								# no of instances the program is allowed to fail to solve. If reached, then the script is aborted
 
 echo "run random instances $PROGRAMM_NAME (Tab-separated columns: File, Time in seconds, solution size, recursive steps, first lower bound difference, high degree rules, degree zero rules, extreme reduction rules, degree one rules, degree two rules, domination rules, degree three rules, lower bounds, finished, solution size verified)"
