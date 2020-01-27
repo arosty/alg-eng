6320b2e7e1eb23b628d4db16985b531c92079d5f
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-01-27 10:09:11 +0100

    take out timestamp

--- smac_test.sh
+++ smac_test.sh
@@ -1,1 +1,2 @@
-./smac-v2.10.03-master-778/smac --scenario-file smac-v2.10.03-master-778/vc-test/vc-scenario.txt --seed $(date +"%Y%m%d%H%M")
+./smac-v2.10.03-master-778/smac --scenario-file smac-v2.10.03-master-778/vc-test/vc-scenario.txt --seed 1
+#  $(date +"%Y%m%d%H%M")
