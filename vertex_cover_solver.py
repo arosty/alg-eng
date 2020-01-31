49429cd9c97b9f53cf298f30d07bb79c742db56b
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-01-31 18:43:02 +0100

    try parameters from smac output

--- vertex_cover_solver.py
+++ vertex_cover_solver.py
@@ -34,26 +34,26 @@ if parameter_input_length > 5:
         elif sys.argv[i] == '-dom_opt': dom_opt = int(sys.argv[i+1]) == 1
 else: 
     #max number of kernelization loops allowed for preproccesing kern
-    limit_kern_start = float('inf')
+    limit_kern_start = 311
     #max number of kernelization loops allowed while branching
-    limit_kern_branch = float('inf')
+    limit_kern_branch = 8
     #reduction rules' frequencies
-    f_deg2 = 1
-    f_deg2_heur = 1
-    f_dom = 1
-    f_deg3 = 1
-    f_lp = 1
-    f_clique_lb = 1
-    f_lp_lb = 1
-    lb_opt = True # True if starts with lp
+    f_deg2 = 29
+    f_deg2_heur = 6
+    f_dom = 32
+    f_deg3 = 57
+    f_lp = 8
+    f_clique_lb = 21
+    f_lp_lb = 95
+    lb_opt = False # True if starts with lp
     #constrained:
-    f_clique_lb_c = 1
-    f_lp_lb_c = 1
-    lb_opt_c = True
+    f_clique_lb_c = 11
+    f_lp_lb_c = 11
+    lb_opt_c = False
     #if True, second method of branching is used
     constrained_branching = False
     #if True, domination rule works with flags
-    dom_opt = True
+    dom_opt = False
 
 def add_vertex(vertex):
     """
