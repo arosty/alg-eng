5ddf0c01fc3b3307ae6294d6588b06a7a1bb623e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-02-07 16:06:11 +0100

    calling looper an call run etc, working

--- param_looper.sh
+++ param_looper.sh
@@ -4,6 +4,6 @@ SECONDS=0
 while [ $SECONDS -lt $maxSec ]; do
     python3 random_config.py > params.txt
     parameters=$(<params.txt)
+    rm -f params.txt
     bash run.sh $parameters
-    # rm -f params.txt
 done
