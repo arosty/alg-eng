399d1881029781de2e7a7609d5fecac053244a6e
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-02-08 16:14:46 +0100

    can read txt correctly

--- history/par10.py
+++ history/par10.py
@@ -1,19 +1,33 @@
 import sys
 import pandas as pd
 
-def give_par10(path_file_name):
+def calc_par10(path_file_name):
     stats = pd.read_csv(path_file_name + '.csv')
-    return stats.loc['snap/loc-gowalla_edges.txt.dimacs']
+    stats = pd.to_numeric(stats.time_in_seconds.str.rstrip('s'))
+    stats.fillna(3000, inplace=True) 
+    return sum(stats)
+
+
+def get_params(path_file_name):
+    f = open(path_file_name + '.txt', 'r')
+    print(f.read())
     
 
 current_path = sys.path[0] + "/"
 
 if len(sys.argv) >= 2:
     for file_name in sys.argv[1:]:
-        stats = give_par10(current_path + file_name)
-        print(stats)
-        # print par10
+        print(file_name + ':')
+        par10_val = calc_par10(current_path + file_name)
+        print(par10_val)
+        print('-------')
     
 else:
-    pass
+    for file_name in sys.stdin:
+        file_name = file_name.replace("\r\n",'')
+        print(file_name + ':')
+        par10_val = calc_par10(current_path + file_name)
+        print(par10_val)
+        get_params(current_path + file_name)
+        print('-------')
     # txt file with file names and go through them
