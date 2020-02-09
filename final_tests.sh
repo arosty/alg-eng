d7b278f346ff9673860da9bd550dd74131ff0280
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-02-09 20:43:09 +0100

    new testing

--- final_tests.sh
+++ final_tests.sh
@@ -1,5 +1,5 @@
 # 202002091816
-bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '1' -f_clique_lb_c '5' -f_deg2 '2' -f_deg2_heur '80000' -f_deg3 '3' -f_dom '1' -f_lp '5' -f_lp_lb '1' -f_lp_lb_c '3' -lb_opt '1' -lb_opt_c '0' -limit_kern_branch '2' -limit_kern_start '500'
+# bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '1' -f_clique_lb_c '5' -f_deg2 '2' -f_deg2_heur '80000' -f_deg3 '3' -f_dom '1' -f_lp '5' -f_lp_lb '1' -f_lp_lb_c '3' -lb_opt '1' -lb_opt_c '0' -limit_kern_branch '2' -limit_kern_start '500'
 # 202002082250
 bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '40000' -f_clique_lb_c '5' -f_deg2 '1' -f_deg2_heur '80000' -f_deg3 '5' -f_dom '1' -f_lp '1' -f_lp_lb '1' -f_lp_lb_c '1' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '1' -limit_kern_start '500'
 # 202002091643
@@ -8,3 +8,9 @@ bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '1' -f_clique_l
 bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '40000' -f_clique_lb_c '5' -f_deg2 '1' -f_deg2_heur '80000' -f_deg3 '5' -f_dom '3' -f_lp '5' -f_lp_lb '1' -f_lp_lb_c '5' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '1' -limit_kern_start '500'
 # 202002082308
 bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '40000' -f_clique_lb_c '5' -f_deg2 '3' -f_deg2_heur '1' -f_deg3 '5' -f_dom '1' -f_lp '5' -f_lp_lb '40000' -f_lp_lb_c '1' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '1' -limit_kern_start '500'
+# 202002091757
+bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '10' -f_clique_lb_c '5' -f_deg2 '2' -f_deg2_heur '80000' -f_deg3 '3' -f_dom '2' -f_lp '3' -f_lp_lb '1' -f_lp_lb_c '3' -lb_opt '1' -lb_opt_c '0' -limit_kern_branch '1' -limit_kern_start '500'
+# 202002091527
+bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '10' -f_clique_lb_c '5' -f_deg2 '2' -f_deg2_heur '80000' -f_deg3 '5' -f_dom '1' -f_lp '5' -f_lp_lb '10' -f_lp_lb_c '3' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '2' -limit_kern_start '500'
+# 202002090834
+bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '1' -f_clique_lb_c '1' -f_deg2 '3' -f_deg2_heur '1' -f_deg3 '1' -f_dom '1' -f_lp '40000' -f_lp_lb '40000' -f_lp_lb_c '1' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '1' -limit_kern_start '3'
