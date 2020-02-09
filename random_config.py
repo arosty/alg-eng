0bbef0ece543606b3fe5f3373c5f7429b1d25a04
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-02-09 12:20:32 +0100

    random choice of lb opt c correction

--- random_config.py
+++ random_config.py
@@ -17,6 +17,6 @@ constrained_branching = [1] #[0, 1]
 dom_opt = 1
 
 output = "-constrained_branching {0} -dom_opt {1} -f_clique_lb {2} -f_clique_lb_c {3} -f_deg2 {4} -f_deg2_heur {5} -f_deg3 {6} -f_dom {7} -f_lp {8} -f_lp_lb {9} -f_lp_lb_c {10} -lb_opt {11} -lb_opt_c {12} -limit_kern_branch {13} -limit_kern_start {14}"
-x = [rd.choice(constrained_branching), dom_opt, rd.choice(f_clique_lb), rd.choice(f_clique_lb_c), rd.choice(f_deg2), rd.choice(f_deg2_heur), rd.choice(f_deg3), rd.choice(f_dom), rd.choice(f_lp), rd.choice(f_lp_lb), rd.choice(f_lp_lb_c), lb_opt, lb_opt_c, rd.choice(limit_kern_branch), rd.choice(limit_kern_start)]
+x = [rd.choice(constrained_branching), dom_opt, rd.choice(f_clique_lb), rd.choice(f_clique_lb_c), rd.choice(f_deg2), rd.choice(f_deg2_heur), rd.choice(f_deg3), rd.choice(f_dom), rd.choice(f_lp), rd.choice(f_lp_lb), rd.choice(f_lp_lb_c), lb_opt, rd.choice(lb_opt_c), rd.choice(limit_kern_branch), rd.choice(limit_kern_start)]
 
 print (output.format(*x))
