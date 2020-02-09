faf0a4b9d379c47f5389d3df50ee0a7705b343cb
Alfons Rosty-Forgách <45817662+arosty@users.noreply.github.com>
2020-02-09 22:46:21 +0100

    continue

--- final_tests.sh
+++ final_tests.sh
@@ -1,7 +1,7 @@
 # 202002091816
 # bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '1' -f_clique_lb_c '5' -f_deg2 '2' -f_deg2_heur '80000' -f_deg3 '3' -f_dom '1' -f_lp '5' -f_lp_lb '1' -f_lp_lb_c '3' -lb_opt '1' -lb_opt_c '0' -limit_kern_branch '2' -limit_kern_start '500'
 # 202002082250
-bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '40000' -f_clique_lb_c '5' -f_deg2 '1' -f_deg2_heur '80000' -f_deg3 '5' -f_dom '1' -f_lp '1' -f_lp_lb '1' -f_lp_lb_c '1' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '1' -limit_kern_start '500'
+# bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '40000' -f_clique_lb_c '5' -f_deg2 '1' -f_deg2_heur '80000' -f_deg3 '5' -f_dom '1' -f_lp '1' -f_lp_lb '1' -f_lp_lb_c '1' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '1' -limit_kern_start '500'
 # 202002091643
 bash run.sh -constrained_branching '1' -dom_opt '1' -f_clique_lb '1' -f_clique_lb_c '5' -f_deg2 '2' -f_deg2_heur '1' -f_deg3 '3' -f_dom '1' -f_lp '3' -f_lp_lb '40000' -f_lp_lb_c '5' -lb_opt '1' -lb_opt_c '1' -limit_kern_branch '2' -limit_kern_start '500'
 # 202002082325
